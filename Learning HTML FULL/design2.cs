6a99d39f6daea7a4e864b3fa554ff1cf48125a9c
IvyMasterDiego <45220823+IvyMasterDiego@users.noreply.github.com>
2019-10-10 11:17:37 -0400

    Add files via upload

---
+++ Learning HTML FULL/design2.cs
@@ -0,0 +1,186 @@
+ this.btn1.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.ForeColor = System.Drawing.Color.Black;
+            this.btn1.Location = new System.Drawing.Point(3, 3);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 70);
+            this.btn1.TabIndex = 5;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            this.btn1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btn1_MouseDoe += new System.Windows.Forms.MouseEventHandler(this.btn1_MouseMove);
+            this.btn1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btn1_MouseUp);
+            // 
+            // btn0
+            // 
+            this.btn0.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn0.ForeColor = System.Drawing.Color.Black;
+            this.btn0.Location = new System.Drawing.Point(84, 231);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(75, 70);
+            this.btn0.TabIndex = 6;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            this.btn0.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btn0_MouseDo this.btn0.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btn0_MouseMove);
+            this.btn0.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btn0_MouseUp);
+            // 
+            // btn_astrerisco
+            // 
+            this.btn_astrerisco.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_astrerisco.ForeColor = System.Drawing.Color.Black;
+            this.btn_astrerisco.Location = new System.Drawing.Point(3, 231);
+            this.btn_astrerisco.Name = "btn_astrerisco";
+            this.btn_astrerisco.Size = new System.Drawing.Size(75, 70);
+            this.btn_astrerisco.TabIndex = 7;
+            this.btn_astrerisco.Text = "*";
+            this.btn_astrerisco.UseVisualStyleBackColor = true;
+            this.btn_astrerisco.Click += new System.EventHandler(this.btn_astrerisco_Click);
+            this.btn_astrerisco.MouseDown += new System.Windows.Forms.MouseEventHandler();
+            
+        
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.ForeColor = System.Drawing.Color.Black;
+            this.btn9.Location = new System.Drawing.Point(165, 155);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(75, 70);
+            this.btn9.TabIndex = 8;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            this.btn9.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btn9_MouseDo this.btn9.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btn9_MouseMove);
+            this.btn9.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btn9_MouseUp);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.ForeColor = System.Drawing.Color.Black;
+            this.btn8.Location = new System.Drawing.Point(84, 155);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(75, 70);
+            this.btn8.TabIndex = 9;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            this.btn8.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btn8_MouseDo this.btn8.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btn8_MouseMove);
+            this.btn8.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btn8_MouseUp);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.ForeColor = System.Drawing.Color.Black;
+            this.btn6.Location = new System.Drawing.Point(165, 79);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(75, 70);
+            this.btn6.TabIndex = 10;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            this.btn6.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btn6_MouseDo this.btn6.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btn6_MouseMove);
+            this.btn6.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btn6_MouseUp);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.ForeColor = System.Drawing.Color.Black;
+            this.btn7.Location = new System.Drawing.Point(3, 155);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(75, 70);
+            this.btn7.TabIndex = 11;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            this.btn7.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btn7_MouseDo this.btn7.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btn7_MouseMove);
+            this.btn7.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btn7_MouseUp);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.ForeColor = System.Drawing.Color.Black;
+            this.btn5.Location = new System.Drawing.Point(84, 79);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 70);
+            this.btn5.TabIndex = 12;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            this.btn5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btn5_MouseDo this.btn5.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btn5_MouseMove);
+            this.btn5.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btn5_MouseUp);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.ForeColor = System.Drawing.Color.Black;
+            this.btn4.Location = new System.Drawing.Point(3, 79);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 70);
+            this.btn4.TabIndex = 13;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            this.btn4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btn4_MouseDo this.btn4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btn4_MouseMove);
+            this.btn4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btn4_MouseUp);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.ForeColor = System.Drawing.Color.Black;
+            this.btn3.Location = new System.Drawing.Point(165, 3);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 70);
+            this.btn3.TabIndex = 14;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            this.btn3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btn3_MouseDo this.btn3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btn3_MouseMove);
+            this.btn3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btn3_MouseUp);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.ForeColor = System.Drawing.Color.Black;
+            this.btn2.Location = new System.Drawing.Point(84, 3);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 70);
+            this.btn2.TabIndex = 15;
+            this.btn2.TabStop = false;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Cli this.btn2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btn2_MouseMove);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btn1);
+            this.flowLayoutPanel1.Controls.Add(this.btn2);
+            this.flowLayoutPanel1.Controls.Add(this.btn3);
+            this.flowLayoutPanel1.Controls.Add(this.btn4);
+            this.flowLayoutPanel1.Controls.Add(this.btn5);
+            this.flowLayoutPanel1.Controls.Add(this.btn6);
+            this.flowLayoutPanel1.Controls.Add(this.btn7);
+            this.flowLayoutPanel1.Controls.Add(this.btn8);
+            this.flowLayoutPanel1.Controls.Add(this.btn9);
+            this.flowLayoutPanel1.Controls.Add(this.btn_astrerisco);
+            this.flowLayoutPanel1.Controls.Add(this.btn0);
+            this.flowLayoutPanel1.Controls.Add(this.btn_numero);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(9, 162);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(246, 314);
+            this.flowLayoutPanel1.TabIndex = 16;
+            // 
+            // btn_numero
+            // 
+            this.btn_numero.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_numero.ForeColor = System.Drawing.Color.Black;
+            this.btn_numero.Location = new System.Drawing.Point(165, 231);
+            this.btn_numero.Name = "btn_numero";
+            this.btn_numero.Size = new System.Drawing.Size(75, 70);
+            this.btn_numero.TabIndex = 16;
+            this.btn_numero.Text = "#";
+            this.btn_numero.UseVisualStyleBackColor = true;
+            this.btn_numero.Click += new System.EventHandler(this.btn_numero_Click);
+            this.btn_numero.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btn_numero_MouseDown);
+            
+            this.btn_numero.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btn_numero_MouseMove);
+            this.btn_numero.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btn_numero_MouseUp);
